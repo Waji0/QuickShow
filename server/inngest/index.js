6355afcfe85650e884ab1063283f8523445b65f2
wajahat ali <wajiali744@gmail.com>
2025-08-15 08:14:44 +0500

    nodemail fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import User from "../models/User.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
-import sendEmail from "../configs/modeMailer.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 
 // Create a client to send and receive events
